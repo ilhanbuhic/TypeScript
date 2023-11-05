304bf29a4d844099c66ca5c87434519772c3189f
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-11-05 21:53:43 +0100

    enums

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,19 @@ let sales: number = 123
 let course: string = 'TypeScript'
 let is_published: boolean = true
 /* 
-In TypeScript we don't always have to annotate our variables,
-because the TS compiler can detect the type of our variables based on their value
+    In TypeScript we don't always have to annotate our variables,
+    because the TS compiler can detect the type of our variables based on their value
 
-If we initialise a variable to a number the TS compiler knows that variable is a number,
-so we don't have to annote it, so instead we can just type --> let sales = 123
+    If we initialise a variable to a number the TS compiler knows that variable is a number,
+    so we don't have to annote it, so instead we can just type --> let sales = 123
 */
 // 1. --------------------------------
 
 // ---------- The ANY type ---------
 /*
-In TS, we have a type called ANY, which can represent any kind of values
-So if we declare a value and don't initialize it, the TS complier assumes this
-variable is a type of ANY
+    In TS, we have a type called ANY, which can represent any kind of values
+    So if we declare a value and don't initialize it, the TS complier assumes this
+    variable is a type of ANY
 */
 
 let level
@@ -37,14 +37,16 @@ level = 'a' //And later we can set it to a string
 function render(document: any) {
     console.log(document)
 }
-// In this function, we have an error where document implicitly has ANY type
-// Implicitly means we haven't explicitly set the type of this parameter,
-// so the compiler is guessing the type of this parameter
-// In this case, we have 2 options
-// 1. --> We can annotate this parameter with ANY
-// 2. --> In case we have tons of errors of this kind we don't want to explicitly annotate
-// various parameters to every function with ANY
-// --> There is a nuclear option for that which is not recommended --> Go to tsconfig.json, set 'noImplicitAny' to 'false
+/* 
+    In this function, we have an error where document implicitly has ANY type
+    Implicitly means we haven't explicitly set the type of this parameter,
+    so the compiler is guessing the type of this parameter
+    In this case, we have 2 options
+    1. --> We can annotate this parameter with ANY
+    2. --> In case we have tons of errors of this kind we don't want to explicitly annotate
+    various parameters to every function with ANY
+    --> There is a nuclear option for that which is not recommended --> Go to tsconfig.json, set 'noImplicitAny' to 'false
+*/
 
 // ---------- The ANY type ---------
 
@@ -56,35 +58,65 @@ function render(document: any) {
 
 // Code completion(Intellisense)
 let numbers: number[] = [1, 2, 3]
-// numbers.forEach((n) => n.)
-// After typing 'n.', we can see all the properties and methods of number objects,
-// because the editor knows the type of 'n', it offers code completion, so we can see
-// all the methods of number objects
+/* numbers.forEach((n) => n.)
+    After typing 'n.', we can see all the properties and methods of number objects,
+    because the editor knows the type of 'n', it offers code completion, so we can see
+    all the methods of number objects
+*/
 
 // ---------- Arrays ---------
 
 // ---------- Tuples ---------
 let user: [number, string] = [1, 'Mosh']
 /*
-Tuple is a fixed length array, where each element has a particular type
-We often use them when we work with a pair of values 
-First we open a square brackets and then tell the compiler
-that the first element is going to be a number and the second will be a string
-and then we initialize that variable
-We will get an error in case we try to add the third element in the example from above
+    Tuple is a fixed length array, where each element has a particular type
+    We often use them when we work with a pair of values 
+    First we open a square brackets and then tell the compiler
+    that the first element is going to be a number and the second will be a string
+    and then we initialize that variable
+    We will get an error in case we try to add the third element in the example from above
 */
 
 // Tuples can be useful when we have only 2 values, like key, value pairs
 
-// One of the methods that can be troublesome is the .push() method
-// We can call this method and store the third value in this array
-// and compiler will not complain, this is one of the gaps in TS
+/* One of the methods that can be troublesome is the .push() method
+    We can call this method and store the third value in this array
+    and compiler will not complain, this is one of the gaps in TS
+*/
 
 // ---------- Tuples ---------
 
 // ---------- Enums ---------
+/*
+    Enums are built-in type which represents a list of related constants
+*/
+
+let small = 1
+let medium = 2
+let large = 3
+
+// We can also group these contants using enum
+// We will put memebers inside an enum
+// enum Size{small, medium, large}
+
+// By default, the TS compiler assigns the first memeber the value of 0
+// and other members 1, 2 and so on
+// In case we don't want to use these values, we can explicitly set these values
+enum anotherSize{small = 30, medium = 35, large = 40}
+
+// We can also use string values, but then we have to explicitly set a value for each member
+enum stringifiedSize{small = 's', medium = 'm', large = 'l'}
+
+enum Size{small = 1, medium, large}
+// Using this enum, we can declare a new variable:
+let mySize: Size = Size.medium
+console.log(mySize);
+// If we define our enums using the 'const' keyword, the compiler will generate more optimized code
 
 // ---------- Enums ---------
+
+
+
 
 
 
